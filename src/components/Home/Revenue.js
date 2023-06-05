0913f54b76ebfe2de9db36f926d56332d78ae67a
mohamadsokhango <m.sokhango7@gmail.com>
2023-06-06 00:29:02 +0330

    responsive revenue

--- src/components/Home/Revenue.js
+++ src/components/Home/Revenue.js
@@ -1,154 +1,142 @@
-import React from "react";
-import "./Revenue.css";
-import icon from "../../assets/Home/ikon.svg";
-import homeowner from "../../assets/Home/Homeowner.svg";
-import iconsta from "../../assets/Home/ikon sta.svg";
-import iconhome from "../../assets/Home/ikonhome.svg";
-import iconcoin from "../../assets/Home/Homeownercoin.svg";
-import icon4 from "../../assets/Home/ikon4.svg";
-import icon5 from "../../assets/Home/ikon5.svg";
-import icon6 from "../../assets/Home/ikon6.svg";
-import icon7 from "../../assets/Home/ikon7.svg";
-import icon8 from "../../assets/Home/ikon8.svg";
-import icon9 from "../../assets/Home/ikon9.svg";
-import icon10 from "../../assets/Home/ikon10.svg";
-import homeownerhand from "../../assets/Home/Homeowner hand.svg";
-
+// 
+import React from 'react'
+import './revenue1.css'
+import homeOwner from '../../assets/Home/Homeowner.svg'
+import guy from '../../assets/Home/Homeownercoin.svg'
+import shakehand from '../../assets/Home/Homeowner hand.svg'
+import itemIcon from '../../assets/Home/ikon.svg'
+import itemIcontwo from '../../assets/Home/ikon sta.svg'
+import itemIconthree from '../../assets/Home/ikonhome.svg'
+import iconfour from '../../assets/Home/ikon4.svg'
+import iconfive from '../../assets/Home/ikon5.svg'
+import iconsix from '../../assets/Home/ikon6.svg'
+import iconseven from '../../assets/Home/ikon7.svg'
+import itemIconeight from '../../assets/Home/ikon8.svg'
+import itemIconnine from '../../assets/Home/ikon9.svg'
+import itemIconten from '../../assets/Home/ikon10.svg'
 const Revenue = () => {
   return (
-    <div className="revenue">
-      <div className="rev-head-body">
-      <p className="rev-head">Neden OpenRoof?</p>
-      <div className="wrap-row-one">
-        <img className="home-icon" src={homeowner} alt="..." />
-        <div className="wrap-icon-box">
-          <div className="wrap-box one">
-            <img className="icon-class" src={icon} alt="icon" />
-            <div className="description">
-              <p className="desc-head">Panel kurulum maliyetini üstleniyoruz</p>
-              <p className="desc-body">
-                Maliyetin istediğiniz kadarına ortak olarak, elektrik
-                faturasından tasarruf etmek ile para kazanmak arasında bir
-                tercih yapabilirsiniz.
-              </p>
+    <>
+    <div className='revenue'>
+      {/* <div className='empity'></div>
+      <div className='empity'></div> */}
+      <div className='home-item-wrapper'>
+        <img className='homeOwner' src={homeOwner} alt='homeOwner' />
+        <div className='item-wrapper'>
+          <div className='item' id='first'>
+            <img className='item-icon' src={itemIcon} alt='itemIcon'/>
+            <div className='item-title'>
+               Panel kurulum maliyetini üstleniyoruz
+            </div>
+            <div className='item-desc'>
+              Maliyetin istediğiniz kadarına ortak olarak, elektrik faturasından
+              tasarruf etmek ile para kazanmak arasında bir tercih
+              yapabilirsiniz.
+            </div>
+           
+          </div>
+          <div className='item' id='second'>
+          <img className='item-icon' src={itemIcontwo} alt='itemIcon'/>
+          <div className='item-title'>
+             Evin satış değerini artırıyoruz
+          </div>
+            <div className='item-desc'>
+            Elektrik faturası ödemeyen, hatta kendi kendine para kazandıran
+              bir ev satışına imkan sağlıyoruz.
             </div>
           </div>
-          <div className="wrap-box two">
-            <img className="icon-class" src={iconsta} alt="icon" />
-            <div className="description">
-              <p className="desc-head">Evin satış değerini artırıyoruz</p>
-              <p className="desc-body">
-                Elektrik faturası ödemeyen, hatta kendi kendine para kazandıran
-                bir ev satışına imkan sağlıyoruz.
-              </p>
+          <div className='item' id='third'>
+            <img className='item-icon' src={itemIconthree} alt='itemIcon'/>
+            <div className='item-title'>
+                Panelleri devrediyoruz
             </div>
-          </div>
-          <div className="wrap-box three">
-            <img className="icon-class" src={iconhome} alt="icon" />
-            <div className="description">
-              <p className="desc-head">Panelleri devrediyoruz</p>
-              <p className="desc-body">
+            <div className='item-desc'>
                 Sözleşme sonunda tüm sistemi ve gelirleri mülk sahibine
                 devrediyoruz.
-              </p>
             </div>
           </div>
         </div>
-      </div>
-      <hr className="line" />
-      <div className="wrap-row-one">
-        <img className="coin-icon-reverse" src={iconcoin} alt="..." />
-        <div className="wrap-icon-box">
-          <div className="wrap-box four">
-            <div className="description">
-              <p className="desc-head">Hızlı amorti süresi</p>
-              <p className="desc-body">
-                Güneş doğmaya devam ettikçe 5-6 yıl içinde yatırımınızı geri
-                alabilirsiniz.
-              </p>
-            </div>
-
-            <img className="icon-class" src={icon4} alt="icon" />
-          </div>
-          <div className="wrap-box five">
-            <div className="description">
-              <p className="desc-head">Gerçek üretim temelli</p>
-              <p className="desc-body">
-                Yatırımınız gerçek üretimden beslendiği için spekülatif
-                risklerden uzak, sağlam bir temele oturtuyoruz.
-              </p>
-            </div>
-            <img className="icon-class" src={icon5} alt="icon" />
-          </div>
-          <div className="wrap-box six">
-            <div className="description">
-              <p className="desc-head">Minimal risk</p>
-              <p className="desc-body">
-                Yatırımcıyı koruyan sözleşmemiz sayesinde ana paranızı geri
-                almanızı garanti ediyoruz!
-              </p>
-            </div>
-            <img className="icon-class" src={icon6} alt="icon" />
-          </div>
-          <div className="wrap-box seven">
-            <div className="description">
-              <p className="desc-head">Yatırım anından itibaren şeffaflık</p>
-              <p className="desc-body">
-                GES projesinin akıbeti ve aylık geri ödemelerin raporlanması
-                gibi tüm süreçlerde para akışlarını şeffaflıkla paylaşıyoruz.
-              </p>
-            </div>
-            <img className="icon-class" src={icon7} alt="icon" />
-          </div>
-        </div>
-        <img className="coin-icon" src={iconcoin} alt="..." />
-      </div>
-
-      <hr className="line" />
-
-      <div className="wrap-row-final">
-        <img className="hand-icon" src={homeownerhand} alt="..." />
-        <div className="wrap-icon-box-three">
-          <div className="wrap-box eight">
-            <img className="icon-class" src={icon8} alt="icon" />
-            <div className="description">
-              <p className="desc-head">Yeşil bir gelecek yaratma imkanı</p>
-              <p className="desc-body">
-                Sürdürülebilir enerjiye geçerek doğaya verilen zararı minimize
-                etmeyi hedefliyoruz.
-              </p>
-            </div>
-          </div>
-          <div className="wrap-box nine">
-            <img className="icon-class" src={icon9} alt="icon" />
-            <div className="description">
-              <p className="desc-head">Tüm detaylarla biz ilgileniyoruz</p>
-              <p className="desc-body">
-                Kurulum firması, kurulum gücü, kullanılacak ekipmanlar, başvuru
-                süreçleri, kar dağıtımı, ve benzeri tüm bürokratik, teknik ve
-                operasyonel konuları biz üstleniyoruz.
-              </p>
-            </div>
-          </div>
-
-          <div className="wrap-box ten">
-            <img className="icon-class" src={icon10} alt="icon" />
-            <div className="description">
-              <p className="desc-head">Eşsiz Yatırım Fırsatı</p>
-              <p className="desc-body">
-                Yatırımcı ve mülk sahibini bir araya getirerek, yenilenebilir
-                enerji sektöründe benzersiz bir yatırım imkanı sunuyoruz.
-                Kapsamlı sözleşmemiz ile tarafların haklarını güvence altına
-                alarak, yenilenebilir enerjiye erişim ve katılımı
-                kolaylaştırıyoruz.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
       </div>
     </div>
-  );
-};
+   
+    <div className='guy'>
+      <div className='guy-item-wrapper'>
+        <img className='guy-mobile' src={guy} alt='guy'/>
+        <div className='item-wrapper-two'>
+          <div className='itemtwo' id='four'>
+             <img className='item-icon-two' src={iconfour} alt='iconfour'/>
+             <div className='item-title'>Hızlı amorti süresi</div>
+             <div className='item-desc'> Güneş doğmaya devam ettikçe 5-6 yıl içinde yatırımınızı geri
+              alabilirsiniz.</div>
+          </div>
+          <div className='itemtwo' id='five'>
+          <img className='item-icon-two' src={iconfive} alt='iconfive'/>
+          <div className='item-title'>Gerçek üretim temelli</div>
+          <div className='item-desc'> Yatırımınız gerçek üretimden beslendiği için spekülatif risklerden
+              uzak, sağlam bir temele oturtuyoruz.</div>
 
-export default Revenue;
+          </div>
+          <div className='itemtwo' id='six'>
+          <img className='item-icon-two' src={iconsix} alt='iconsix'/>
+          <div className='item-title'>Minimal risk</div>
+          <div className='item-desc'> Yatırımcıyı koruyan sözleşmemiz sayesinde ana paranızı geri
+              almanızı garanti ediyoruz!</div>
+
+          </div>
+          <div className='itemtwo' id='seven'>
+          <img className='item-icon-two' src={iconseven} alt='iconseven'/>
+          <div className='item-title'>Yatırım anından itibaren şeffaflık</div>
+          <div className='item-desc'>  GES projesinin akıbeti ve aylık geri ödemelerin raporlanması gibi
+              tüm süreçlerde para akışlarını şeffaflıkla paylaşıyoruz.</div>
+          </div>
+        </div>
+        <img className='guy-img' src={guy} alt='guy'/>
+      </div>
+    </div>
+    
+    <div className='shake'>
+      {/* <div className='empity'></div>
+      <div className='empity'></div> */}
+      <div className='home-item-wrapper'>
+        <img className='homeOwner' src={shakehand} alt='homeOwner' />
+        <div className='item-wrapper'>
+          <div className='item' id='first'>
+            <img className='item-icon' src={itemIconeight} alt='itemIcon'/>
+            <div className='item-title'>
+            Gerçek üretim temelli
+            </div>
+            <div className='item-desc'>
+            Yatırımınız gerçek üretimden beslendiği için spekülatif risklerden
+              uzak, sağlam bir temele oturtuyoruz.
+            </div>
+           
+          </div>
+          <div className='item' id='second'>
+          <img className='item-icon' src={itemIconnine} alt='itemIcon'/>
+          <div className='item-title'>
+          Minimal risk
+          </div>
+            <div className='item-desc'>
+            Yatırımcıyı koruyan sözleşmemiz sayesinde ana paranızı geri
+              almanızı garanti ediyoruz!
+            </div>
+          </div>
+          <div className='item' id='third'>
+            <img className='item-icon' src={itemIconten} alt='itemIcon'/>
+            <div className='item-title'>
+            Yatırım anından itibaren şeffaflık
+            </div>
+            <div className='item-desc'>
+            GES projesinin akıbeti ve aylık geri ödemelerin raporlanması gibi
+              tüm süreçlerde para akışlarını şeffaflıkla paylaşıyoruz.
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </>
+  )
+}
+
+export default Revenue
