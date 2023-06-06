89290481e2c2420efb56a5a8bcac2388c81f68b4
moji <mojtabaparvar@gmail.com>
2023-06-06 16:06:53 +0330

    chore: add form and some minor fixes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
 import "./Home.css";
 import home from "../../assets/Header/Hero görsel.svg";
-import backGround from "../../assets/Header/hero_background.svg";
-import backGroundback from "../../assets/Home/hero background back.svg";
 import ko from "../../assets/Header/ok0.svg";
 import Metin from "./Metin";
 import Benefit from "./Benefit";
 import Revenue from "./Revenue";
-import WildLife from "./WildLife";
-import AskComponents from "./AskComponents";
 
 const Home = () => {
   return (
