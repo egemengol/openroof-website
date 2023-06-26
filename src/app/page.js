140358d2feb72945d5c00c3fd4854129874009d0
denizmatar <deniz@mentholprotocol.com>
2023-06-26 20:06:09 +0300

    carousel text alignment

--- src/app/page.js
+++ src/app/page.js
@@ -54,7 +54,7 @@ export default function Home() {
       <Benefit />
       <Revenue />
       {/* <WildLife /> */}
-      <CarouselLogo />
+      {/* <CarouselLogo /> */}
       <RevenueSharing />
       {/* <AskComponents /> */}
       <CarouselMoreInfo />
